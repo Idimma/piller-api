0673be146b1327056876a81f3e7e26672f0f2c3d
Idowu Emmanuel <idowu.immanuel17@gmail.com>
2020-08-30 22:06:31 +0100

    Removing Dirts

--- resources/views/pages/deposit-plan.blade.php
+++ resources/views/pages/deposit-plan.blade.php
@@ -12,12 +12,12 @@
                 <div class="overlay">
                     <div class="Add-Card-box">
                         <div class="Add-card-box-header">
-                            <h2> <span class="underline">ADD</span>CARD</h2>
-                            <img src="../assets/images/cancel.svg" class="x-button" alt="">
+                            <h2><span class="underline">ADD</span>CARD</h2>
+                            <img src="{{asset('assets/images/cancel.svg')}}" class="x-button" alt="">
                         </div>
                         <p class="invalid">Invalid Card, Please check if your card has expired</p>
-                        <form action="" class="Add-card-box-form">
-
+                        <form action="{{url('card')}}" method="post" class="Add-card-box-form">
+                            @csrf
                             <div class="form-group-full">
                                 <div class="form-group-header">
                                     <h2>Name your card</h2>
@@ -40,7 +40,8 @@
                                 <div class="form-group-header">
                                     <h2>Card Number</h2>
                                 </div>
-                                <input type="text" class="form-input-full credit-card" placeholder="1234-XXXX-XXXX-XXXX" maxlength="19">
+                                <input type="text" class="form-input-full credit-card" placeholder="1234-XXXX-XXXX-XXXX"
+                                       maxlength="19">
                                 <p class="additional-info">
                                     The bold number on your Debit Card
                                 </p>
@@ -61,24 +62,25 @@
                         </form>
                     </div>
                 </div>
-                <form action="/pages/" method="POST" class="NewPlanForm normal-deposit-form show-form">
+                <form action="{{url('plan')}}" method="POST" class="NewPlanForm normal-deposit-form show-form">
+                    @csrf
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>Plan Name  <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
+                            <h2>Plan Name <span class="Important"><img
+                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>
                             <img class="question-mark" src="" alt="">
                         </div>
-                        <input type="text" class="form-input-full">
+                        <input name="plan_name" type="text" class="form-input-full">
                         <p class="input-detail">Kindly give your plan a name</p>
                     </div>
-
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>Choose building type<span class="Important"><img src="" alt=""></span> </h2>
+                            <h2>Choose building type<span class="Important"><img src="" alt=""></span></h2>
                         </div>
                         <div class="special-dropdown">
                             <div class="value-holder">
                                 <span class="Value-text">Choose building type</span>
-                                <img src="../assets/images/dropDown.svg" alt="">
+                                <img src="{{asset('assets/images/dropDown.svg')}}" alt="">
                             </div>
                             <div class="options-holder">
                                 <ul>
@@ -87,78 +89,78 @@
                                     <li class="drop-down-option">Building Type 3</li>
                                 </ul>
                             </div>
-                            <input type="text" class="hidden-input" name="Card">
+                            <input type="text" class="hidden-input" name="building_type">
                         </div>
                         <p class="input-detail">Kindly select a building plan</p>
                     </div>
-
-
                     <div class="form-group-full">
                         <div class="form-group-header">
                             <h2>Estimation of materials needed</h2>
                         </div>
-                        <input type="text" class="form-input-full" placeholder="1000 Units of Blocks, 100 Bags of Cement">
+                        <input type="text" class="form-input-full" name="material_estimation"
+                               placeholder="1000 Units of Blocks, 100 Bags of Cement">
                     </div>
-
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>Deposit frequency   <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
+                            <h2>Deposit frequency <span class="Important"><img
+                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>
                         </div>
                         <div class="special-dropdown">
                             <div class="value-holder">
                                 <span class="Value-text">Daily</span>
-
-                                <img src="../assets/images/dropDown.svg" alt="">
+                                <img src="{{asset('assets/images/dropDown.svg')}}" alt="">
                             </div>
                             <div class="options-holder">
                                 <ul>
                                     <li class="drop-down-option">Daily</li>
                                     <li class="drop-down-option">Weekly</li>
                                     <li class="drop-down-option">Monthly</li>
-
                                 </ul>
                             </div>
-                            <input type="text" class="hidden-input" name="Deposit frequency">
+                            <input type="text" class="hidden-input" value="Daily" name="deposit_frequency">
                         </div>
                         <p class="input-detail">Select how often you want to make a deposit</p>
                         <!-- add special type of drop down -->
                     </div>
-
+                    <input hidden name="plan_status" value="PENDING">
+                    <input hidden name="plan_type" value="normal">
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>Choose Materials   <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
+                            <h2>Choose Materials <span class="Important"><img
+                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>
                         </div>
                         <div class="select-group">
                             <div class="radio-group">
-                                <input type="radio" name="connected" id="Blocks">
+                                <input value="blocks" type="radio" name="material_type" id="Blocks">
                                 <label for="Blocks">Blocks only</label>
                             </div>
 
                             <div class="radio-group">
-                                <input type="radio" name="connected" id="cement">
+                                <input value="cement" type="radio" name="material_type" id="cement">
                                 <label for="cement">Cement only</label>
                             </div>
-
                             <div class="radio-group">
-                                <input type="radio" name="connected" id="both">
+                                <input value="both" type="radio" name="material_type" id="both">
                                 <label for="both">both</label>
                             </div>
 
                         </div>
                     </div>
+
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>Stokkpile target  <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
+                            <h2>Stokkpile target <span class="Important"><img
+                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>
                         </div>
                         <div class="units-of">
                             <div class="half-groups">
                                 <label for="blocks-unit">Unit of Blocks</label>
-                                <input type="number" min="1" id="blocks-unit" class="half-input" >
+                                <input type="number" min="1" name="block_target" id="blocks-unit" class="half-input">
                             </div>
 
                             <div class="half-groups">
                                 <label for="cement-unit">Bags of Cement</label>
-                                <input type="number" min="1" id="cement-unit" class="half-input">
+                                <input type="number" min="1" name="cement_target" id="cement-unit" class="half-input">
                             </div>
 
                         </div>
@@ -166,7 +168,8 @@
 
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>Set priority for Building Materials  <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
+                            <h2>Set priority for Building Materials <span class="Important"><img
+                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>
                         </div>
                         <div class="slideContainer">
                             <div class="top-slider">
@@ -179,7 +182,7 @@
                             </div>
                             <div class="sliderContainerInner">
                                 <div class="sliderBackground"></div>
-                                <input type="range" min="0" max="100" value=0 class="slider" id="myRange">
+                                <input name="block_percent" type="range" min="0" max="100" value=0 class="slider" id="myRange">
                             </div>
                             <div class="bottom-slider">
                                 <span class="left">units of blocks</span>
@@ -190,20 +193,22 @@
 
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>When is your start date?  <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
+                            <h2>When is your start date? <span class="Important"><img
+                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>
                         </div>
-                        <input type="date" class="form-input-full">
+                        <input type="date" name="start_date" class="form-input-full">
                     </div>
 
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>Stockpile Country  <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
+                            <h2>Stockpile Country <span class="Important"><img
+                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>
                         </div>
                         <div class="special-dropdown">
                             <div class="value-holder">
                                 <span class="Value-text">Nigeria</span>
 
-                                <img src="../assets/images/dropDown.svg" alt="">
+                                <img src="{{asset('assets/images/dropDown.svg')}}" alt="">
                             </div>
                             <div class="options-holder">
                                 <ul>
@@ -213,43 +218,45 @@
 
                                 </ul>
                             </div>
-                            <input type="text" class="hidden-input" name="stockpile-Country">
-                            <p class="input-detail">Kindly choose a stokkpile country</p>
-                        </div>
-                        <!-- add special type of drop down -->
-                    </div>
-                    <div class="form-group-full">
-                        <div class="form-group-header">
-                            <h2>Select Card  <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
-                        </div>
-                        <div class="special-dropdown">
-                            <div class="value-holder">
-                                <span class="Value-text">Add a card</span>
-
-                                <img src="../assets/images/dropDown.svg" alt="">
-                            </div>
-                            <div class="options-holder">
-                                <ul>
-                                    <li class="drop-down-option Add-card-Trigger">Add a card</li>
-                                    <li class="drop-down-option">Business</li>
-
-                                </ul>
-                            </div>
-                            <input type="text" class="hidden-input" name="Card">
-                            <p class="input-detail">Kindly choose a pre-registered Debit Card</p>
+                            <input type="text" class="hidden-input" value="Nigeria" name="country">
+                            <p class="input-detail">Kindly choose a Stokkpile country</p>
                         </div>
                         <!-- add special type of drop down -->
                     </div>
 
+{{--                    <div class="form-group-full">--}}
+{{--                        <div class="form-group-header">--}}
+{{--                            <h2>Select Card <span class="Important"><img--}}
+{{--                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>--}}
+{{--                        </div>--}}
+{{--                        <div class="special-dropdown">--}}
+{{--                            <div class="value-holder">--}}
+{{--                                <span class="Value-text">Add a card</span>--}}
+
+{{--                                <img src="{{asset('assets/images/dropDown.svg')}}" alt="">--}}
+{{--                            </div>--}}
+{{--                            <div class="options-holder">--}}
+{{--                                <ul>--}}
+{{--                                    <li class="drop-down-option Add-card-Trigger">Add a card</li>--}}
+{{--                                    <li class="drop-down-option">Business</li>--}}
+{{--                                </ul>--}}
+{{--                            </div>--}}
+{{--                            <input type="text" class="hidden-input" name="Card">--}}
+{{--                            <p class="input-detail">Kindly choose a pre-registered Debit Card</p>--}}
+{{--                        </div>--}}
+{{--                        <!-- add special type of drop down -->--}}
+{{--                    </div>--}}
+
                     <div class="form-group-full">
                         <div class="form-group-header">
-                            <h2>Auto Deposit  <span class="Important"><img src="../assets/images/Reason for saving.svg" alt=""></span> </h2>
+                            <h2>Auto Deposit <span class="Important"><img
+                                            src="{{asset('assets/images/Reason for saving.svg')}}" alt=""></span></h2>
                         </div>
                         <div class="wierdDiv">
                             <div class="currencySymbol">
                                 $
                             </div>
-                            <input type="text" class="form-input-full wierdInput">
+                            <input type="text" name="deposit" class="form-input-full wierdInput">
                         </div>
                         <p class="input-detail">The amount you want to deposit according to your deposit frequency</p>
                     </div>
